eba5db8b0af1c9e28c815bb760e6f0ba119b70e2
Christopher Espiritu <christopheredrian@gmail.com>
2018-06-02 07:10:35 +0800

    Completed assessment

--- assessment.js
+++ assessment.js
@@ -12,15 +12,15 @@ function helloWorld() {
 
 /*
 2. Write a function called lambdaSchool that has a single parameter called num.
-     num will be a positive integer.
-     If num is divisible by 3 return the string 'Lambda'
-     If num is divisible by 5 return the string 'School'
-     If num is divisible by 3 AND 5 return the string 'Lambda School' (notice the space)
-     If num is NOT divisible by 3 or 5 then return num.
+num will be a positive integer.
+If num is divisible by 3 return the string 'Lambda'
+If num is divisible by 5 return the string 'School'
+If num is divisible by 3 AND 5 return the string 'Lambda School' (notice the space)
+If num is NOT divisible by 3 or 5 then return num.
 
-     Example:
-             lambdaSchool(15); // returns 'Lambda School'
-             lambdaSchool(8); // returns 8
+Example:
+lambdaSchool(15); // returns 'Lambda School'
+lambdaSchool(8); // returns 8
 */
 
 function lambdaSchool(num) {
@@ -31,19 +31,19 @@ function lambdaSchool(num) {
   } else if (num % 5 === 0 ){
     return "School";
   } else {
-   return num;
+    return num;
   }
 }
 
 /*
 3. Write a function called longestString that has a single parameter called strs.
-     strs will be an array of strings.
-     Return the longest string that is in strs.
-     If there is a tie for the longest string then return the one that occurs first.
+strs will be an array of strings.
+Return the longest string that is in strs.
+If there is a tie for the longest string then return the one that occurs first.
 
-     Example:
-             longestString(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
-             longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
+Example:
+longestString(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
+longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
 */
 
 function longestString(strs) {
@@ -58,23 +58,23 @@ function longestString(strs) {
 
 /*
 4. Write a function called computeUserAverageAge that has a single parameter called users
-     users is an array of user objects.
-     Each user object has a property called age that is a number.
-     Compute the average age of all user objects in the users array.
-     Round off the decimals if needed and return the number.
+users is an array of user objects.
+Each user object has a property called age that is a number.
+Compute the average age of all user objects in the users array.
+Round off the decimals if needed and return the number.
 
-     Example:
-             const users = [{
-               name: 'Brendan Eich',
-               age: 56,
-             }, {
-               name: 'Linus Torvalds',
-               age: 48,
-             }, {
-               name: 'Margaret Hamilton',
-               age: 81,
-             }];
-             computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
+Example:
+const users = [{
+  name: 'Brendan Eich',
+  age: 56,
+}, {
+  name: 'Linus Torvalds',
+  age: 48,
+}, {
+  name: 'Margaret Hamilton',
+  age: 81,
+}];
+computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
 */
 
 function computeUserAverageAge(users) {
@@ -85,8 +85,8 @@ function computeUserAverageAge(users) {
 }
 
 module.exports = {
- helloWorld,
- lambdaSchool,
- longestString,
- computeUserAverageAge
+  helloWorld,
+  lambdaSchool,
+  longestString,
+  computeUserAverageAge
 };
